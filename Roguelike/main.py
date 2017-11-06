46a568f73d0d0324ae0aa836a0a84f1ae3780506
Dmitry Belousov <Demonss11@gmail.com>
2017-11-06 13:31:57 +0400

    первые проблески карты

--- Roguelike/main.py
+++ Roguelike/main.py
@@ -5,6 +5,28 @@ import pygame
 #game files
 import constants
 
+"""
+  _______      ___      .___  ___.  _______ 
+ /  _____|    /   \     |   \/   | |   ____|
+|  |  __     /  ^  \    |  \  /  | |  |__   
+|  | |_ |   /  /_\  \   |  |\/|  | |   __|  
+|  |__| |  /  _____  \  |  |  |  | |  |____ 
+ \______| /__/     \__\ |__|  |__| |_______|
+                                            
+"""
+
+class struc_Title:
+    def __init__(self, block_path):
+        self.block_path = block_path
+
+
+def map_create():
+    new_map = [[struc_Title(False) for y in range(0,constants.MAP_WIDTH)] for x in range(0,constants.MAP_HEIGHT)]
+    new_map[10][10].block_path = True
+    new_map[10][15].block_path = True
+
+    return new_map
+
 
 
 
@@ -16,6 +38,7 @@ def draw_game():
     SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
 
     #TODO draw the map
+    draw_map(GAME_MAP)
 
     #draw the character
     SURFACE_MAIN.blit(constants.S_PLAYER, ( 200, 200 ))
@@ -23,7 +46,13 @@ def draw_game():
     #update the display
     pygame.display.flip()
 
-
+def draw_map(map_to_draw):
+    for x in range(0,constants.MAP_HEIGHT):
+        for y in range(0,constants.MAP_WIDTH):
+            if map_to_draw[x][y].block_path == True:
+                SURFACE_MAIN.blit(constants.S_WALL, (x*constants.CELL_HEIGHT,y*constants.CELL_WIDTH))
+            else:
+                SURFACE_MAIN.blit(constants.S_FLOOR, (x*constants.CELL_HEIGHT,y*constants.CELL_WIDTH))
 
 
 
@@ -59,14 +88,14 @@ def game_main_loop():
 def game_initialize():
     '''This function initializes the main window, and pygame'''
 
-    global SURFACE_MAIN
+    global SURFACE_MAIN, GAME_MAP
 
     #initialize pygame
     pygame.init()
 
     SURFACE_MAIN = pygame.display.set_mode( (constants.GAME_WIDTH, constants.GAME_HEIGHT) )
 
-
+    GAME_MAP = map_create()
 
 
 
